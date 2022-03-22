ef1557c87ca093302ed2597820e6b427da5cf692
Luis Garzon <quanluis@protonmail.com>
2022-03-21 21:01:56 -0400

    Created a for loop that would put each individual movie based on genre into it's proper ArrayList. Also created a basic menu to navigate the movie list the user wants to see.

--- src/movieGenres/ProcessMovieFiles.java
+++ src/movieGenres/ProcessMovieFiles.java
@@ -20,7 +20,6 @@ public class ProcessMovieFiles {
 		Creators process = new Creators();
 		movies processTwo = new movies();
 		
-		
 		try {
 			
 			File movieFile = new File(this.file);
@@ -45,23 +44,92 @@ public class ProcessMovieFiles {
 			
 			for(int i =0; i < length; i++) {
 	
+				// Find war movies and put them into separate ArrayList 
 				
-				System.out.println(processTwo.movies.get(i));
-	
+				if(processTwo.movies.get(i).contains("Sci Fi") == true){
+					
+				//	System.out.println(processTwo.movies.get(i));
+					
+					processTwo.movieGenreSciFi.add(processTwo.movies.get(i));
+
+				} else if (processTwo.movies.get(i).contains("Adventure") == true) {
+					
+					processTwo.movieGenreAdventure.add(processTwo.movies.get(i));
+					
+
+				} else if (processTwo.movies.get(i).contains("Drama") == true) {
+					
+					processTwo.movieGenreDrama.add(processTwo.movies.get(i));
+					
+				}	
+				  else if (processTwo.movies.get(i).contains("War") == true) {
+					  
+					  processTwo.movieGenreWar.add(processTwo.movies.get(i));
+  	
+				}
+				  else if (processTwo.movies.get(i).contains("Romance")) {
+					  
+					  
+					  processTwo.movieGenreRomance.add(processTwo.movies.get(i));
+
+				}
+				  else if(processTwo.movies.get(i).contains("Thriller")) {
+					  
+					  processTwo.movieGenreThriller.add(processTwo.movies.get(i));
+					  
+				}
+				  else if (processTwo.movies.get(i).contains("Fantasy")) {
+					  
+					  processTwo.movieGenreFantasy.add(processTwo.movies.get(i));
+
+				  }
 				
 			}
-			
+	
+	
 			String output = "Director: " + process.creators.get(0) + "\n"
 					+ "Composer: " + process.creators.get(1) + "\n\n"
 					
 					+ "Genres: " +  null + "\n\n"
 					+ "Movie Title:   " + "Year Released:   " + "Rating: " ;
 			
+			String input = "Enter a Genre: \n"
+							+ "1: Adventure\n2: Drama \n3: War \n4: Romance \n5: Thriller \n6: Fantasy \n7: Exit";
+			
+			
+			String option = JOptionPane.showInputDialog(null, input);
+			
+			
+			int choice = Integer.parseInt(option);
+			
+			switch (choice) {
+				case 1: 
+					System.out.println(processTwo.movieGenreAdventure);			
+				break;
+				case 2: 
+					System.out.println(processTwo.movieGenreDrama);
+				break;
+				case 3:
+					System.out.println(processTwo.movieGenreWar);
+				break;
+				case 4:
+					
+					processTwo.trimRomance();
+					
+				break;
+				case 5:
+					System.out.println(processTwo.movieGenreThriller);
+					
+				break;
+				case 6:
+					System.out.println(processTwo.movieGenreFantasy);
+				break;
+				case 7:
+					System.exit(1);
+				break;
+			}
 			
 			JOptionPane.showMessageDialog(null, output);
-			
-			//System.out.println(processTwo.movieGenreWar);
-			
 			
 			run.close();
 			
@@ -70,7 +138,8 @@ public class ProcessMovieFiles {
 			e.printStackTrace();
 		}
 		
-		}
+	}
+
 		
 	}
 
--- src/movieGenres/movies.java
+++ src/movieGenres/movies.java
@@ -5,6 +5,28 @@ import java.util.ArrayList;
 public class movies extends Creators {
 	
 	ArrayList<String> movies = new ArrayList<>();
+	ArrayList<String> movieGenreWar = new ArrayList<>();
 	ArrayList<String> movieGenreThriller = new ArrayList<>();
+	ArrayList<String> movieGenreSciFi = new ArrayList<>();
+	ArrayList<String> movieGenreRomance = new ArrayList<>();
+	ArrayList<String> movieGenreAdventure = new ArrayList<>();
+	ArrayList<String> movieGenreDrama = new ArrayList<>();
+	ArrayList<String> movieGenreFantasy = new ArrayList<>();
+	
+	
+	public String movieTitle;
+	public String yearReleased;
+	public String Genre;
+	public String rating;
+	
+	
+	public void trimRomance() {
+		
+		
+		//
+		System.out.println("Output" + movieGenreRomance.get(0));
+		System.out.println(movieGenreRomance);
+		
+	}
 	
 }
